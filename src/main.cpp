64ea157f749c4c96045d9ba818b8c29419f06d31
Anecoz <heilmair5@gmail.com>
2018-12-07 21:25:28 +0100

    Contribs and cmake project up and running

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <asio.hpp>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main()
+{
+  GLFWwindow* window;
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  window = glfwCreateWindow(1280, 720, "BossEyebrow", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return -2;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
